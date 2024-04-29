af62a8fc5a8012bfc6ec53c1c14fe18a0087d558
Bertram Carlsen <Bertramcarlsen@gmail.com>
2024-04-29 16:24:01 +0200

    Commit

--- main.js
+++ main.js
@@ -15,3 +15,28 @@ window.onload = function () {
         mobile_menu.classList.toggle('is-active');
     })
 }
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Stop the form from submitting the traditional way
+
+    var formData = new FormData(this);
+
+    fetch(this.action, {
+        method: 'POST',
+        body: formData
+    }).then(response => {
+        // Handle the response from sendE.php
+        if(response.ok) {
+            // Hide the form
+            document.getElementById('myForm').style.display = 'none';
+            // Show the thank-you message
+            document.getElementById('thankYouMessage').style.display = 'block';
+        } else {
+            // Handle errors
+            alert('There was a problem submitting your form. Please try again.');
+        }
+    }).catch(error => {
+        // Handle any other errors
+        alert('There was a problem submitting your form. Please try again.');
+    });
+});
