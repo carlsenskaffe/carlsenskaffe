a8bbff46570d7c27db008d23b7a4a675d5ae8a36
Bertram Carlsen <Bertramcarlsen@gmail.com>
2024-06-26 22:18:47 +0200

    Commit SEO

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <meta name="description" content="kaffecykel til leje, mobil kaffebar og kaffe catering">
+    <meta name="keywords" content="Kaffecykel, Mobil kaffebar, kaffe catering">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Carlsens Kaffe</title>
+    <title>Carlsens Kaffecykel</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://app.weply.chat/widget/26481c0966b89ed01c88a998c1fe83ff" async></script>
+    <script src="main.js" defer></script>
     <link rel="icon" type="image/ico" href="assets/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 <body>
 
@@ -18,7 +21,7 @@
         <div class="container">
             <div class="logo_div">
                 <a href="index.html">
-                    <img src="assets/logo.png" width="75" height="75" class="logo">
+                    <img src="assets/logo.png" width="75" height="75" class="logo" title="Carlsens kaffe logo" alt="Carlsens kaffe logo" loading="lazy">
                 </a>
             </div>
 
@@ -32,31 +35,27 @@
             <button class="hamburger">
                 <div class="bar"></div>
             </button>
-           
+        </div>
     </header>
+    
     <nav class="mobile-nav">
         <a class="nav_a" href="#drinks-menu">Menu</a>
         <a class="nav_a" href="#faq-section">Spørgsmål</a>
         <a class="nav_a" href="#prices">Priser</a>
         <a class="nav_a" href="#contact">Forespørgsel</a>
-        
     </nav>
 
     <div class="hero">
         <div class="herogradient">
-            <img src="assets/hero.jpg" class="hero-image">
+            <img src="assets/kaffecykel.webp" class="hero-image" title="mobil kaffebar" alt="Carlsens kaffecykel på havnen" loading="lazy">
         </div>
-            <div class="hero-content" id="hero-content">
+        <div class="hero-content" id="hero-content">
             <h1>Lej en kaffecykel med barista i København</h1>
             <a href="#contact">
                 <button class="hero-button" id="hero-button">Få et uforpligtende tilbud</button>
             </a>
         </div> 
     </div>
-
-    
-    
-    
 
     <section class="info-section" id="info-section">
         <div class="section-title">
@@ -82,71 +81,67 @@
                 <h3>Skræddersyede events</h3>
                 <p>Står du for et større event, og vil du ikke vil have kaffe ad libitum, kan vi sagtens stå og sælge til dit event. Læs mere <a class="info-mail" href="#faq-section">her</a></p>
             </div>
-            
         </div>
     </section>
-    
-
 
     <section class="drinks-menu" id="drinks-menu">
-        <h2 Class="Vores-Menu" id ="IdMenu">Vores Menu</h2>
+        <h2 class="Vores-Menu" id="IdMenu">Vores Menu</h2>
         <div class="drink-item">
-            <img src="assets/svg/Espresso.svg" alt="Espresso" class="drink-icon">
+            <img src="assets/svg/Espresso.svg" alt="Espresso" class="drink-icon" title="Espresso" loading="lazy">
             <h3>Espresso</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Cortado.svg" alt="Cortado" class="drink-icon">
+            <img src="assets/svg/Cortado.svg" alt="Cortado" class="drink-icon" title="Cortado" loading="lazy">
             <h3>Cortado</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Flat White.svg" alt="FlatWhite" class="drink-icon">
+            <img src="assets/svg/Flat White.svg" alt="FlatWhite" class="drink-icon" title="Flat White" loading="lazy">
             <h3>Flat white</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Americano.svg" alt="Americano" class="drink-icon">
+            <img src="assets/svg/Americano.svg" alt="Americano" class="drink-icon" title="Americano" loading="lazy">
             <h3>Americano</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Caffe Latte.svg" alt="CaffeLatte" class="drink-icon">
+            <img src="assets/svg/Caffe Latte.svg" alt="CaffeLatte" class="drink-icon" title="Caffe Latte" loading="lazy">
             <h3>Caffe latte</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Cappuccino.svg" alt="Cappuccino" class="drink-icon">
+            <img src="assets/svg/Cappuccino.svg" alt="Cappuccino" class="drink-icon" title="Cappuccino" loading="lazy">
             <h3>Cappuccino</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Iskaffe.svg" alt="Iskaffe" class="drink-icon2">
+            <img src="assets/svg/Iskaffe.svg" alt="Iskaffe" class="drink-icon2" title="Iskaffe" loading="lazy">
             <h3>Iskaffe m. valgfri sirup*</h3>
         </div>
-        
         <div class="drink-item">
-            <img src="assets/svg/Varm Kakao.svg" alt="VarmKakao" class="drink-icon">
+            <img src="assets/svg/Varm Kakao.svg" alt="VarmKakao" class="drink-icon" title="Varm Kakao" loading="lazy">
             <h3>Varm kakao</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Chai Latte.svg" alt="ChaiLatte" class="drink-icon">
+            <img src="assets/svg/Chai Latte.svg" alt="ChaiLatte" class="drink-icon" title="Chai Latte" loading="lazy">
             <h3>Chai latte</h3>
         </div>
         <div class="drink-item">
-            <img src="assets/svg/Te.svg" alt="Te" class="drink-icon">
+            <img src="assets/svg/Te.svg" alt="Te" class="drink-icon" title="Økologisk Valgfri Te" loading="lazy">
             <h3>Økologisk valgfri te</h3>
         </div>
-        <a class="karamelsirup">*Karamelsirup er standard, men skriv gerne hvilken sirup dine gæster ville foretrække.</a>
+        <p class="karamelsirup">*Karamelsirup er standard, men skriv gerne hvilken sirup dine gæster ville foretrække.</p>
     </section>
 
     <section id="faq-section" class="faq-section">
         <div class="image-container">
-            <img src="assets/2.jpg">
+            <img src="assets/mobil_kaffebar.webp" title="Mobil kaffebar" alt="Mobil kaffebar" loading="lazy">
         </div>
         <div class="faq-content">
             <h2>Ofte Stillede Spørgsmål</h2>
             <div class="faq-item">
-                <h3>Hvilken slags events laver i?</h3>
-                <p>Vi kommer med glæde til alle slags events. Alt fra firmaevents og private fester til sportsbegivenheder og messer.<br>Hvis du står for et større event kommer vi med glæde ud og sælger kaffe til en god pris. Vi tager imod MobilePay og kort. For at høre nærmere om denne mulighed, så skriv en mail til os <a class="faq-p" href="mailto:carlsenskaffe@gmail.com">her</span></a></p>
+                <h3>Hvilken slags events laver I?</h3>
+                <p>Vi kommer med glæde til alle slags events. Alt fra firmaevents og private fester til sportsbegivenheder og messer.<br>Hvis du står for et større event, kommer vi med glæde ud og sælger kaffe til en god pris. Vi tager imod MobilePay og kort. For at høre nærmere om denne mulighed, så skriv en mail til os <a class="faq-p" href="mailto:carlsenskaffe@gmail.com">her</a></p>
             </div>
             <div class="faq-item">
                 <h3>Hvor stor er kaffecyklen?</h3>
-                <p>Kaffecyklen er ca. 90 cm. bred og ca. 225 cm. lang, og kan derfor komme ind og stå næsten alle steder.</p>
+                <p>Kaffecyklen er ca. 90 cm bred og ca. 225 cm lang, og kan derfor komme ind og stå næsten alle steder.</p>
             </div>
             <div class="faq-item">
                 <h3>Hvad hvis det regner?</h3>
@@ -154,18 +149,10 @@
             </div>
             <div class="faq-item">
                 <h3>Hvad skal kaffecyklen bruge?</h3>
-                <p>
-                    Kaffecyklen er udstyret med egen strøm, vand, gas, kalkfilter og køleboks.
-                    <br>Køleboksen har plads til 8 liter mælk.
-                </p>
+                <p>Kaffecyklen er udstyret med egen strøm, vand, gas, kalkfilter og køleboks.<br>Køleboksen har plads til 8 liter mælk.</p>
             </div>
-            <div class="faq-item">
-                <h3></h3>
-            </div>
-            
         </div>
     </section>
-    
 
     <section id="prices">
         <h2 class="priser-title">Vores priser</h2>
@@ -194,17 +181,16 @@
                 <div class="time">6 timers fri kaffe</div>
                 <div class="price">6.500 kr,-</div>
             </div>
-    </div>
-    <div class="footer-prices">
-        <p>Alle priser er eksklusiv moms.</p>
-        <p>Et transportgebyr tillægges for events uden for København.<br>Eksempler: Roskilde, Køge, Hillerød: 1.000 kr,-</p>
-    </div>
-
+        </div>
+        <div class="footer-prices">
+            <p>Alle priser er eksklusiv moms.</p>
+            <p>Et transportgebyr tillægges for events uden for København.<br>Eksempler: Roskilde, Køge, Hillerød: 1.000 kr,-</p>
+        </div>
     </section>
 
     <section class="contact" id="contact">
         <form id="contact-form" method="post" action="sendE.php">
-            <h1>Lav en forespørgsel på dit kaffeevent helt uforpligtende.</h1>
+            <h1>Lav en forespørgsel på dit kaffe event helt uforpligtende.</h1>
             <div class="inline-inputs">
                 <div>
                     <label for="fornavn">Fornavn</label>
@@ -215,8 +201,6 @@
                     <input type="text" name="efternavn" id="efternavn" required>
                 </div>
             </div>
-            
-
             <label for="email">Email</label>
             <input type="email" name="email" id="email" required>
 
@@ -258,11 +242,9 @@
             <textarea name="beskrivEvent" id="beskrivEvent"></textarea>
             
             <button type="submit">Send forespørgsel</button>
-            
         </form>
-        
         <div class="image-container">
-            <img src="assets/3.jpg">
+            <img src="assets/kaffe_catering.webp" title="Kaffe catering" alt="Kaffe catering" loading="lazy">
         </div>
     </section>
 
@@ -275,31 +257,28 @@
                     <li><a href="#faq-section">Spørgsmål</a></li>
                     <li><a href="#prices">Priser</a></li>
                     <li><a href="#contact">Forespørgsel</a></li>
-                    
                 </ul>
             </div>
             <div class="footer-column">
                 <h4>Kontakt Os</h4>
                 <ul>
-                    <li><a>Carlsens Kaffe</a></li>
-                    <li><a>Strindbergsvej 68</a></li>
-                    <li><a>2500 Valby</a></li>
+                    <li>Carlsens Kaffe</li>
+                    <li>Strindbergsvej 68</li>
+                    <li>2500 Valby</li>
                     <li><a href="mailto:carlsenskaffe@gmail.com">Carlsenskaffe@gmail.com</a></li>
-                    <li><a>CVR: 44425165</a></li>
+                    <li>CVR: 44425165</li>
                 </ul>
             </div>
             <div class="footer-column">
                 
                 <ul class="social-media">
-                    <li><a href="https://www.instagram.com/carlsenskaffe" target="_blank"><img class="instapadding" src="assets/svg/instagram.svg" alt="Instagram"></a></li>
-                    <li><a href="https://www.findsmiley.dk/1411083" target ="_blank"><img src="assets/Smiley.png"></a></li>
+                    <li><a href="https://www.instagram.com/carlsenskaffe" target="_blank"><img class="instapadding" src="assets/svg/instagram.svg" title="Carlsens kaffecykel instagram" alt="Instagram"></a></li>
+                    <li><a href="https://www.findsmiley.dk/1411083" target ="_blank"><img src="assets/Smiley.png" title="findsmiley.dk" alt="findsmiley.dk"></a></li>
                     <li><a href="https://www.iubenda.com/privacy-policy/51529802" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privatlivspolitik ">Privatlivspolitik</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script></li>
                 </ul>
             </div>
         </div>
-        <script src="https://app.weply.chat/widget/26481c0966b89ed01c88a998c1fe83ff" async></script>
     </footer>
 
-   <script src="main.js"></script>
 </body>
 </html>
